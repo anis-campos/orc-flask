d26889c25da0f9f2d30765e216ba7a0d4fbfdda2
Anis Campos <yocorporation@hotmail.com>
2020-02-24 19:51:54 +0100

    adding model

--- fbapp/config.py
+++ fbapp/config.py
@@ -1,2 +1,7 @@
+import os
+
 SECRET_KEY = '@@9\r!6Zr7\'"bt-=\nY\nBQ1A\rh'
 FB_API_ID = "525369721720539"
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
