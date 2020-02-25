f5c160014ae5a660d106051d8008c2a825461a57
Anis Campos <yocorporation@hotmail.com>
2020-02-25 08:15:42 +0100

    adding base template and using partials

--- fbapp/views.py
+++ fbapp/views.py
@@ -12,5 +12,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/result/')
+def result():
+    return render_template('result.html')
+
+
 if __name__ == "__main__":
     app.run()
