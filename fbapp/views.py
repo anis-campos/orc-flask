ca8ba828fdaf738e06dfa96d7b53797b95a691bf
Anis Campos <yocorporation@hotmail.com>
2020-02-25 11:06:09 +0100

    adding open graph metadata

--- fbapp/views.py
+++ fbapp/views.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, url_for, request
 
 from . import config
 
-
 app = Flask(__name__)
 
 app.config.from_object(config)
@@ -14,10 +13,18 @@ def index():
     description = """
          Toi, tu n'as pas peur d'être seul ! Les grands espaces et les aventures sont faits pour toi. D'ailleurs, Koh Lanta est ton émission préférée ! Bientôt tu partiras les cheveux au vent sur ton radeau. Tu es aussi un idéaliste chevronné. Quelle chance !
      """
+    page_title = "Le test ultime"
+    og_url = url_for('index', _external=True)
+    og_image = url_for('static', filename='tmp/sample.jpg')
+    og_description = "Découvre qui tu es vraiment en faisant le test ultime !"
     return render_template('index.html',
                            user_name='Julio',
                            user_image=url_for('static', filename='img/profile.png'),
                            description=description,
+                           og_url=og_url,
+                           page_title=page_title,
+                           og_image=og_image,
+                           og_description=og_description,
                            blur=True)
 
 
